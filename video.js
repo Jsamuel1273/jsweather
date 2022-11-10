6afae38ecd4ea7e6f46ceffe9b791cf0272d8e8c
Jsamuel1273 <109645212+Jsamuel1273@users.noreply.github.com>
2022-11-10 14:47:58 +1100

    last one

--- video.js
+++ video.js
@@ -1,2 +1,2 @@
-const vide = ["../videos/Sunset - 8451.mp4", "../videos/Sunrise - 7127.mp4", "../videos/Footage - 12127.mp4"]
+const vide = ["../videos/Sunset - 8451.mp4", "../videos/Footage - 12127.mp4", "../videos/Tree - 31461.mp4" ]
  module.exports = vide;
